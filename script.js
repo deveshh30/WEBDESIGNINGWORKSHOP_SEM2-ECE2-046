e7555e09d40dfee48968ea61ff5edb379060de3a
deveshh30 <devesh30gupta@gmail.com>
2026-04-08 00:17:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+        var msg=" hello  ABES";
+        console.log(msg);
+        // console.log(tyoe of (msg));
+        let mob= 8374566873456;
+        console.log(mob);
+         const email="info@email.com";
+        console.log(email);
+        function demo(a,b){
+            var num1=a;
+            var num2=b;
+            var res=num1+num2;
+            console.log("addition is"+ res);
+        
